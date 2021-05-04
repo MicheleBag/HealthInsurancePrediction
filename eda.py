96c74d84ade61d2c24e13045fc6a94184ecfb241
MicheleBag <33416572+MicheleBag@users.noreply.github.com>
2021-05-04 12:53:13 +0200

    First commit

---
+++ eda.py
@@ -0,0 +1,13 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+
+DATASET_NAME = 'HealthInsurance - Dataset.csv'
+dataset = pd.read_csv(DATASET_NAME)
+print(dataset.head())
+
+# Controllo se nel dataset sono presenti eventuali valori NaN da gestire
+check_nan = dataset.isnull().values.any()
+count_nan = dataset.isnull().sum().sum()
+print('NaN values: ' + str(check_nan))
+print('NaN count: ' + str(count_nan))
+
