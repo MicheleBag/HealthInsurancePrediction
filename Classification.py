0adcb4e7545a0d6747792df1c0eda7ae476e4f2a
MicheleBag <baglionimichele24@gmail.com>
2021-06-03 12:01:16 +0200

    Removed KNN, implemented LogisticReg and NN + code review

--- Classification.py
+++ Classification.py
@@ -1,93 +1,122 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_score, precision_score, recall_score, \
+    roc_curve
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
+from imblearn.over_sampling import SMOTE
 
-"""
-TODO:
-- Precision&recall --> BAD RESULTS
-- try different model?
-- L1/L2 normalization?
-"""
+
+def model_scores(y_test, y_pred, y_pred_proba):
+    # Evaluation metrics
+    print('Confusion matrix :\n', confusion_matrix(y_test, y_pred))
+    print('Accuracy :', accuracy_score(y_test, y_pred))
+    print('AUC score :', roc_auc_score(y_test, y_pred_proba))
+    print('F1-score :', f1_score(y_test, y_pred))
+    print('Precision score :', precision_score(y_test, y_pred))
+    print('Recall score :', recall_score(y_test, y_pred))
+    # ROC
+    fpr_test, tpr_test, threshold_test = roc_curve(y_test, y_pred_proba)
+    plt.figure()
+    plt.plot(fpr_test, fpr_test)
+    plt.plot(fpr_test, tpr_test)
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.title('ROC')
+
+
+def model_evaluation(model, X_train, y_train, X_test, y_test, trained=False):
+    if not trained:
+        model.fit(X_train, y_train)
+    # Predict on train set
+    y_pred = model.predict(X_train)
+    y_pred_proba = model.predict_proba(X_train)[::, 1]
+    print('Train dataset :')
+    model_scores(y_train, y_pred, y_pred_proba)
+    # Predict on test set
+    y_pred = model.predict(X_test)
+    y_pred_proba = model.predict_proba(X_test)[::, 1]
+    print('\nTest dataset :')
+    model_scores(y_test, y_pred, y_pred_proba)
+
 
 DATASET_NAME = 'Dataset.csv'
 TEST_SIZE = .2
 VALIDATION_SIZE = .2
 dataset = pd.read_csv(DATASET_NAME)
 
-#print(dataset.info())
+# print(dataset.info())
 pd.set_option('display.max_columns', None)
-#print(dataset.head())
+# print(dataset.head())
 dataset.drop('id', axis=1, inplace=True)
 
-# Splitto il dataset in training e test
+# Split dataset into train and test set
 y = dataset['Response'].values
 X = dataset.drop('Response', axis=1).values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=1, stratify=y)
-# Controllo se le etichette positive sono proporzionate tra train e test
-#print("Train/Test shape:", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+# Check if label are balanced
+# print("Train/Test shape:", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 # print("Test set % of labels == 1:", y_test.sum() / y.sum() * 100)
 
-# Splitto il training set in training+validation set
-# X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=VALIDATION_SIZE, random_state=1, stratify=y_train)
 
+# Logistic Regression
+log_reg = LogisticRegressionCV(Cs=5, cv=5)
+print("\n##### LOGISTIC REGRESSION #####")
+# model_evaluation(log_reg, X_train, y_train, X_test, y_test)
 
-# Stratified K-Fold cross validation
-k_fold = [5]  # , 10]
-neighbors_range = range(1, 20)
+# Dataset oversampling
+smote = SMOTE(sampling_strategy='minority')
+X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
+#  Check if label are balanced
+# print("Oversampled train shape:", X_train_sm.shape, y_train_sm.shape)
+# print("Original train shape:", X_train.shape, y_train.shape)
+# print("#1 in oversampled train:", y_train_sm.sum())
+# print("#1 in original train:", y_train.sum())
+# Labels are equally distributed
 
-for k in k_fold:
-    skf = StratifiedKFold(n_splits=k, shuffle=True)
-    scores = [0]*(max(neighbors_range))
-    precision = [0]*(max(neighbors_range))
-    recall = [0]*(max(neighbors_range))
+# Logistic Regression with oversampled train
+print("\n##### LOGISTIC REGRESSION (oversampled data) #####")
+# model_evaluation(log_reg, X_train_sm, y_train_sm, X_test, y_test)
 
-    for train_index, val_index in skf.split(X_train, y_train):
-        print("TRAIN:", train_index, "VAL:", val_index)
-        X_train2, X_val = X_train[train_index], X_train[val_index]
-        y_train2, y_val = y_train[train_index], y_train[val_index]
+"""
+# Split training set into training+validation set
+X_train2, X_val, y_train2, y_val = train_test_split(X_train, y_train, test_size=VALIDATION_SIZE,
+                                                    random_state=1, stratify=y_train)
+"""
 
-        # KNN
-        for n in neighbors_range:
-            knn = KNeighborsClassifier(n_neighbors=n)
-            knn.fit(X_train2, y_train2)
-            y_pred = knn.predict(X_val)
-            #scores[n-1] += metrics.f1_score(y_val, y_pred, pos_label=1)
-            precision[n-1] += metrics.precision_score(y_val, y_pred, pos_label=1)
-            recall[n-1] += metrics.recall_score(y_val, y_pred, pos_label=1)
-            #print(scores)
-            # fpr, tpr, thresholds = metrics.roc_curve(y_val, y_pred, pos_label=1)
-            #metrics.plot_confusion_matrix(knn, X_val, y_val)
-            #metrics.plot_roc_curve(knn, X_val, y_val)
-            #plt.show()
+# Neural network parameters
+parameter_space = {
+    'hidden_layer_sizes': [(30,), (50, 50, 50), (50, 100, 50), (100,)],
+    'alpha': [0.0001, 0.1, 1, 3],
+    'learning_rate': ['constant', 'adaptive'],
+}
 
-    # Evaluation metrics
-    # print(fpr, tpr, thresholds)
+# Neural network
+print("\n##### NEURAL NETWORKS #####")
+mpl = MLPClassifier(max_iter=500)
 
-    #scores = np.array(scores)
-    #scores = scores / k_fold
-    #print(scores)
-    # plt.figure()
-    # plt.plot(neighbors_range, scores)
-    # plt.xlabel('K')
-    # plt.ylabel('F1-Score')
+# Searching best parameters
+clf = GridSearchCV(mpl, parameter_space, n_jobs=-1, cv=5)
+clf.fit(X_train, y_train)
+# Best parameter set
+print('Best parameters found:\n', clf.best_params_)
+# {'alpha': 0.0001, 'hidden_layer_sizes': (100,), 'learning_rate': 'constant'}
 
-    precision = np.array(precision)
-    recall = np.array(recall)
-    precision = precision / k_fold
-    recall = recall / k_fold
-    print(precision)
-    print(recall)
-    plt.figure()
-    plt.plot(neighbors_range, precision)
-    plt.xlabel('K')
-    plt.ylabel('Precision')
-    plt.figure()
-    plt.plot(neighbors_range, recall)
-    plt.xlabel('K')
-    plt.ylabel('Recall')
+model_evaluation(clf, X_train, y_train, X_test, y_test, trained=True)
+
+# Neural network with oversampled train
+print("\n##### NEURAL NETWORKS (oversampled data) #####")
+# Searching best parameters
+clf = GridSearchCV(mpl, parameter_space, n_jobs=-1, cv=5)
+clf.fit(X_train_sm, y_train_sm)
+# Best parameter set
+print('Best parameters found:\n', clf.best_params_)
+#  {'alpha': 0.0001, 'hidden_layer_sizes': (50, 100, 50), 'learning_rate': 'adaptive'}
+model_evaluation(clf, X_train_sm, y_train_sm, X_test, y_test, trained=True)
 
 plt.show()
--- eda.py
+++ eda.py
@@ -18,31 +18,31 @@ print(dataset.info())
 print(dataset.head())
 # print(dataset.tail())
 
-# Controllo se nel dataset sono presenti eventuali valori NaN da gestire
+# Check NaN value
 check_nan = dataset.isnull().values.any()
 count_nan = dataset.isnull().sum().sum()
 print('NaN values: ', check_nan)
 print('NaN count: ', count_nan)
 
-# Rimuovo eventuali campioni duplicati
+# Remove duplicated samples
 print('Dataset shape: ', dataset.shape)
 duplicate_rows = dataset[dataset.duplicated()]
 print('#Duplicated rows: ', duplicate_rows.shape)
 
-# Dummizzo la features non numeriche
+# Dummyfying non numerical features
 columns_to_dummy = ['Gender', 'Vehicle_Age', 'Vehicle_Damage']
 dataset = pd.get_dummies(dataset, columns=columns_to_dummy)
 print(dataset.head(10))
 
-# Rimuovo features binarie inutili
+# Remove useless binary features
 columns_to_drop = ['Gender_Female', 'Vehicle_Damage_No']
 dataset.drop(columns_to_drop, axis=1, inplace=True)
 
-# Rinomino features complementari a quelle rimosse
+# Rename complementary features of deleted ones
 dataset.rename(columns={'Gender_Male': 'Gender', 'Vehicle_Damage_Yes': 'Vehicle_Damage'}, inplace=True)
 print(dataset.info())
 
-# Controllo il bilanciamento delle feature nel dataset
+# Check for unbalanced features
 columns_to_check = ['Age', 'Driving_License', 'Region_Code', 'Previously_Insured', 'Policy_Sales_Channel',
                     'Gender', 'Vehicle_Damage', 'Vehicle_Age_1-2 Year',
                     'Vehicle_Age_< 1 Year', 'Vehicle_Age_> 2 Years', 'Response']
@@ -52,17 +52,17 @@ for column in columns_to_check:
     plt.figure()
     dataset[column].value_counts(normalize=True).plot.pie()
 
-# Rimuovo la feature Driving_License poichè praticamente tutti i campioni nel
-# dataset hanno la patente di guida:
+
+# Dropping features Driving_License because almost all sample has driving license:
 # 1 = 0.998 && 0 = 0.002
 dataset.drop('Driving_License', axis=1, inplace=True)
 
-# Controllo collinearità tra features
+# Check collinearity between features
 corrMatrix = dataset.corr()
 plt.figure()
 sn.heatmap(corrMatrix, annot=True)
 
-# Controllo se il premio è correlato all'età dell'assicurato -> NO
+# Checking if Annual_Premium is correlated with Age -> NO
 plt.figure()
 plt.scatter(dataset['Age'], dataset['Annual_Premium'])
 
@@ -71,11 +71,11 @@ columns_to_boxplot = ['Age', 'Region_Code', 'Annual_Premium', 'Policy_Sales_Chan
 plt.figure()
 dataset.boxplot(columns_to_boxplot)
 
-# Verifico HLP per la feature Annual_Premium
+# Checking for HLP for Annual_Premium feature
 plt.figure()
 dataset.boxplot('Annual_Premium')
 
-# Rimuovo l'1% dei valori più grandi
+# Removing 1% of the highest values
 dataset = dataset[(dataset.Annual_Premium < dataset.Annual_Premium.quantile(.99))]
 plt.figure()
 dataset.boxplot('Annual_Premium')
@@ -87,6 +87,6 @@ dataset[features_to_normalize] = scaler.fit_transform(dataset[features_to_normal
 #print(dataset.head())
 
 
-#plt.show()
+plt.show()
 dataset.to_csv('Dataset.csv', index=False)
 
