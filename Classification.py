60b21c35fbbae0cff4d185c699cf6d9580bb7dd1
MicheleBag <baglionimichele24@gmail.com>
2021-06-24 11:35:34 +0200

    Added GridSearch

--- Classification.py
+++ Classification.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_score, precision_score, recall_score, \
     roc_curve
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.neural_network import MLPClassifier
-from sklearn import metrics
 from imblearn.over_sampling import SMOTE
 
+# Sklearn design matrix
+#   TN  FP
+#   FN  TP
 
-def model_scores(y_test, y_pred, y_pred_proba):
+
+def model_scores(y_test, y_pred, y_pred_proba, show_plot):
     # Evaluation metrics
     print('Confusion matrix :\n', confusion_matrix(y_test, y_pred))
     print('Accuracy :', accuracy_score(y_test, y_pred))
@@ -22,12 +23,18 @@ def model_scores(y_test, y_pred, y_pred_proba):
     print('Recall score :', recall_score(y_test, y_pred))
     # ROC
     fpr_test, tpr_test, threshold_test = roc_curve(y_test, y_pred_proba)
-    plt.figure()
+    if show_plot:
+        plt.subplot(1, 2, 2)
+        plt.title('ROC-AUC: test set')
+    else:
+        plt.subplot(1, 2, 1)
+        plt.title('ROC-AUC: training set')
     plt.plot(fpr_test, fpr_test)
     plt.plot(fpr_test, tpr_test)
     plt.xlabel('FPR')
     plt.ylabel('TPR')
-    plt.title('ROC')
+    if show_plot:
+        plt.figure()
 
 
 def model_evaluation(model, X_train, y_train, X_test, y_test, trained=False):
@@ -37,22 +44,20 @@ def model_evaluation(model, X_train, y_train, X_test, y_test, trained=False):
     y_pred = model.predict(X_train)
     y_pred_proba = model.predict_proba(X_train)[::, 1]
     print('Train dataset :')
-    model_scores(y_train, y_pred, y_pred_proba)
+    model_scores(y_train, y_pred, y_pred_proba, show_plot=False)
     # Predict on test set
     y_pred = model.predict(X_test)
     y_pred_proba = model.predict_proba(X_test)[::, 1]
     print('\nTest dataset :')
-    model_scores(y_test, y_pred, y_pred_proba)
+    model_scores(y_test, y_pred, y_pred_proba, show_plot=True)
 
 
 DATASET_NAME = 'Dataset.csv'
 TEST_SIZE = .2
-VALIDATION_SIZE = .2
 dataset = pd.read_csv(DATASET_NAME)
 
-# print(dataset.info())
 pd.set_option('display.max_columns', None)
-# print(dataset.head())
+print(dataset.head())
 dataset.drop('id', axis=1, inplace=True)
 
 # Split dataset into train and test set
@@ -62,12 +67,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, r
 # Check if label are balanced
 # print("Train/Test shape:", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 # print("Test set % of labels == 1:", y_test.sum() / y.sum() * 100)
-
+# Label are balanced
 
 # Logistic Regression
-log_reg = LogisticRegressionCV(Cs=5, cv=5)
 print("\n##### LOGISTIC REGRESSION #####")
-# model_evaluation(log_reg, X_train, y_train, X_test, y_test)
+parameter_space = {
+    'Cs': [1, 3, 5, 10],
+    'cv': [3, 5]
+}
+log_reg = LogisticRegressionCV(parameter_space)
+clf = GridSearchCV(log_reg, parameter_space, n_jobs=-1, cv=5)
+clf.fit(X_train, y_train)
+print('Best parameters found:\n', clf.best_params_)
+model_evaluation(clf, X_train, y_train, X_test, y_test, trained=True)
+
 
 # Dataset oversampling
 smote = SMOTE(sampling_strategy='minority')
@@ -81,42 +94,43 @@ X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
 
 # Logistic Regression with oversampled train
 print("\n##### LOGISTIC REGRESSION (oversampled data) #####")
-# model_evaluation(log_reg, X_train_sm, y_train_sm, X_test, y_test)
+clf = GridSearchCV(log_reg, parameter_space, n_jobs=-1, cv=5)
+clf.fit(X_train_sm, y_train_sm)
+print('Best parameters found:\n', clf.best_params_)
+model_evaluation(clf, X_train_sm, y_train_sm, X_test, y_test, trained=True)
 
-"""
-# Split training set into training+validation set
-X_train2, X_val, y_train2, y_val = train_test_split(X_train, y_train, test_size=VALIDATION_SIZE,
-                                                    random_state=1, stratify=y_train)
-"""
 
 # Neural network parameters
 parameter_space = {
-    'hidden_layer_sizes': [(30,), (50, 50, 50), (50, 100, 50), (100,)],
-    'alpha': [0.0001, 0.1, 1, 3],
+    'hidden_layer_sizes': [(8, 5), (7,), (10, 3), (30,), (50, 50, 50), (50, 100, 50), (100,)],
+    'alpha': [0.0001, 0.1, 1, 3, 5],
     'learning_rate': ['constant', 'adaptive'],
 }
 
 # Neural network
 print("\n##### NEURAL NETWORKS #####")
-mpl = MLPClassifier(max_iter=500)
-
+# mpl = MLPClassifier(max_iter=500)
 # Searching best parameters
-clf = GridSearchCV(mpl, parameter_space, n_jobs=-1, cv=5)
-clf.fit(X_train, y_train)
+# clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=5)
+# clf.fit(X_train, y_train)
 # Best parameter set
-print('Best parameters found:\n', clf.best_params_)
-# {'alpha': 0.0001, 'hidden_layer_sizes': (100,), 'learning_rate': 'constant'}
-
-model_evaluation(clf, X_train, y_train, X_test, y_test, trained=True)
+# print('Best parameters found:\n', clf.best_params_)
+# {'alpha': 0.0001, 'hidden_layer_sizes': (100,), 'learning_rate': 'adaptive'}
+# model_evaluation(clf, X_train, y_train, X_test, y_test, trained=True)
+mlp = MLPClassifier(max_iter=500, alpha=0.0001, hidden_layer_sizes=(100,), learning_rate='adaptive')
+model_evaluation(mlp, X_train, y_train, X_test, y_test, trained=False)
 
 # Neural network with oversampled train
 print("\n##### NEURAL NETWORKS (oversampled data) #####")
+# mpl = MLPClassifier(max_iter=500)
 # Searching best parameters
-clf = GridSearchCV(mpl, parameter_space, n_jobs=-1, cv=5)
-clf.fit(X_train_sm, y_train_sm)
+# clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=5)
+# clf.fit(X_train_sm, y_train_sm)
 # Best parameter set
-print('Best parameters found:\n', clf.best_params_)
-#  {'alpha': 0.0001, 'hidden_layer_sizes': (50, 100, 50), 'learning_rate': 'adaptive'}
-model_evaluation(clf, X_train_sm, y_train_sm, X_test, y_test, trained=True)
+# print('Best parameters found:\n', clf.best_params_)
+# {'alpha': 0.0001, 'hidden_layer_sizes': (50, 100, 50), 'learning_rate': 'adaptive'}
+# model_evaluation(clf, X_train_sm, y_train_sm, X_test, y_test, trained=True)
+mlp = MLPClassifier(max_iter=500, alpha=0.0001, hidden_layer_sizes=(50, 100, 50), learning_rate='adaptive')
+model_evaluation(mlp, X_train_sm, y_train_sm, X_test, y_test, trained=False)
 
 plt.show()
--- eda.py
+++ eda.py
@@ -3,12 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 from sklearn import preprocessing
 
-"""
-TODO: 
-    -finire controllo sul bilanciamento delle classi 
-        1 = 0.12 && 0 = 0.88
-    -provare senza auto maggiori a 2 anni
-"""
 
 DATASET_NAME = 'HealthInsurance - OriginalDataset.csv'
 dataset = pd.read_csv(DATASET_NAME)
@@ -21,11 +15,11 @@ print(dataset.head())
 # Check NaN value
 check_nan = dataset.isnull().values.any()
 count_nan = dataset.isnull().sum().sum()
-print('NaN values: ', check_nan)
+print('\n\nNaN values: ', check_nan)
 print('NaN count: ', count_nan)
 
 # Remove duplicated samples
-print('Dataset shape: ', dataset.shape)
+print('\n\nDataset shape: ', dataset.shape)
 duplicate_rows = dataset[dataset.duplicated()]
 print('#Duplicated rows: ', duplicate_rows.shape)
 
@@ -46,11 +40,11 @@ print(dataset.info())
 columns_to_check = ['Age', 'Driving_License', 'Region_Code', 'Previously_Insured', 'Policy_Sales_Channel',
                     'Gender', 'Vehicle_Damage', 'Vehicle_Age_1-2 Year',
                     'Vehicle_Age_< 1 Year', 'Vehicle_Age_> 2 Years', 'Response']
-for column in columns_to_check:
-    #print(column, "count: ", dataset[column].value_counts())
-    #print(column, "Normalized count: ", dataset[column].value_counts(normalize=True))
-    plt.figure()
-    dataset[column].value_counts(normalize=True).plot.pie()
+for idx, value in enumerate(columns_to_check):
+    print("\n", value, "count: ", dataset[value].value_counts())
+    print(value, "Normalized count: ", dataset[value].value_counts(normalize=True))
+    plt.subplot(3, 4, idx+1)
+    dataset[value].value_counts(normalize=True).plot.pie()
 
 
 # Dropping features Driving_License because almost all sample has driving license:
@@ -84,9 +78,7 @@ dataset.boxplot('Annual_Premium')
 features_to_normalize = ['Age', 'Region_Code', 'Annual_Premium', 'Policy_Sales_Channel', 'Vintage']
 scaler = preprocessing.MinMaxScaler()
 dataset[features_to_normalize] = scaler.fit_transform(dataset[features_to_normalize])
-#print(dataset.head())
-
+print(dataset.head())
 
 plt.show()
 dataset.to_csv('Dataset.csv', index=False)
-
